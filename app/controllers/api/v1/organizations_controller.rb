f0b86189877da384f1c6f69573609ca071f4da87
Sujit Shakya <sshakya.mail@gmail.com>
2018-04-24 14:55:51 +0545

    Adds api endpoint to create new organization. After organization is created, tenant will be created.

--- app/controllers/api/v1/organizations_controller.rb
+++ app/controllers/api/v1/organizations_controller.rb
@@ -1,2 +1,13 @@
 class Api::V1::OrganizationsController < ApplicationController
+  def create
+    org = Organization.new(organization_params)
+    if org.save
+      render json: {record: org, message: "organization with its own tenant created successfully"}.to_json, status: 201
+    end
+  end
+
+  private
+  def organization_params
+    params.require(:organization).permit(:name, :subdomain)
+  end
 end
